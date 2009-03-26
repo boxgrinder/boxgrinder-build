1cfc9f767ae5a561c54093d4ae2593cf8f28c909
Marek Goldmann <marek.goldmann@gmail.com>
2009-03-26 13:19:54 +0100

    initial converting to EC2 image type added

--- lib/jboss-cloud/appliance-ec2-image.rb
+++ lib/jboss-cloud/appliance-ec2-image.rb
@@ -19,6 +19,7 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
 require 'rake/tasklib'
+require 'fileutils'
 
 module JBossCloud
   class ApplianceEC2Image < Rake::TaskLib
@@ -27,17 +28,40 @@ module JBossCloud
       @config            = config
       @appliance_config  = appliance_config
       
-      appliance_build_dir     = "#{@config.dir_build}/#{@appliance_config.appliance_path}"
-      @appliance_xml_file     = "#{appliance_build_dir}/#{@appliance_config.name}.xml"
+      @appliance_build_dir       = "#{@config.dir_build}/#{@appliance_config.appliance_path}"
+      @appliance_xml_file        = "#{@appliance_build_dir}/#{@appliance_config.name}.xml"
+      @appliance_ec2_image_file  = "#{@appliance_build_dir}/#{@appliance_config.name}-ec2.img"
       
       define_tasks
     end
     
     def define_tasks
+      file @appliance_ec2_image_file  => [ @appliance_xml_file ] do
+        convert_image_to_ec2_format
+      end
+      
       desc "Build #{@appliance_config.simple_name} appliance for Amazon EC2"
-      task "appliance:#{@appliance_config.name}:ec2" => [ @appliance_xml_file ] do
-        puts "Converting..."
+      task "appliance:#{@appliance_config.name}:ec2" => [ @appliance_ec2_image_file ]
+    end
+    
+    def convert_image_to_ec2_format
+      puts "Converting #{@appliance_config.simple_name} appliance image to EC2 format..."
+      
+      raw_file = "#{@appliance_build_dir}/#{@appliance_config.name}-sda.raw"
+      tmp_dir = "#{@config.dir.build}/appliances/#{@config.build_path}/tmp/ec2-image-#{rand(9999999999).to_s.center(10, rand(9).to_s)}"
+      
+      FileUtils.mkdir_p( tmp_dir )
+      
+      # we're using ec2-converter from thincrust appliance tools (http://thincrust.net/tooling.html)
+      command = "sudo ec2-converter -f #{raw_file} --inputtype diskimage -n #{@appliance_ec2_image_file} -t #{tmp_dir}"
+      
+      exit_status =  execute_command( command )
+      
+      unless exit_status
+        puts "\nConverting #{@appliance_config.simple_name} to EC2 format failed! Hint: consult above messages.\n\r"
+        abort
       end
     end
+    
   end
 end
