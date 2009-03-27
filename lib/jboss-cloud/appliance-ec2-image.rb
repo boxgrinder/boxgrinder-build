b6272c3f9138375601613f1eff6987b17a293e62
Marek Goldmann <marek.goldmann@gmail.com>
2009-03-27 11:07:14 +0100

    EC2 configuration data exported to a file, validation for values, bundling

--- lib/jboss-cloud/appliance-ec2-image.rb
+++ lib/jboss-cloud/appliance-ec2-image.rb
@@ -20,6 +20,8 @@
 
 require 'rake/tasklib'
 require 'fileutils'
+require 'jboss-cloud/validator/errors'
+require 'yaml'
 
 module JBossCloud
   class ApplianceEC2Image < Rake::TaskLib
@@ -28,11 +30,30 @@ module JBossCloud
       @config            = config
       @appliance_config  = appliance_config
       
-      @appliance_build_dir       = "#{@config.dir_build}/#{@appliance_config.appliance_path}"
-      @appliance_xml_file        = "#{@appliance_build_dir}/#{@appliance_config.name}.xml"
-      @appliance_ec2_image_file  = "#{@appliance_build_dir}/#{@appliance_config.name}-ec2.img"
+      @appliance_build_dir          = "#{@config.dir_build}/#{@appliance_config.appliance_path}"
+      @bundle_dir                   = "#{@appliance_build_dir}/ec2/bundle"
+      @appliance_xml_file           = "#{@appliance_build_dir}/#{@appliance_config.name}.xml"
+      @appliance_ec2_image_file     = "#{@appliance_build_dir}/#{@appliance_config.name}-ec2.img"
+      @appliance_ec2_manifest_file  = "#{@bundle_dir}/#{@appliance_config.name}-ec2.img.manifest.xml"
+      
+      @ec2_data_file = "#{ENV['HOME']}/.jboss-cloud/ec2"
       
       define_tasks
+    end
+    
+    def validate_config
+      if File.exists?( @ec2_data_file )
+        @ec2_data = YAML.load_file( @ec2_data_file )
+      else
+        raise ValidationError, "EC2 configuration file (#{@ec2_data_file}), doesn't exists. Please create."
+      end
+      
+      raise ValidationError, "Please specify path to cert in EC2 configuration file (#{@ec2_data_file}). See http://oddthesis.org/theses/jboss-cloud/projects/jboss-cloud-support/pages/ec2-configuration-file for more info." if @ec2_data['cert_file'].nil?
+      raise ValidationError, "Please specify path to private key in EC2 configuration file (#{@ec2_data_file}). See http://oddthesis.org/theses/jboss-cloud/projects/jboss-cloud-support/pages/ec2-configuration-file for more info." if @ec2_data['key_file'].nil?
+      raise ValidationError, "Please specify account number in EC2 configuration file (#{@ec2_data_file}). See http://oddthesis.org/theses/jboss-cloud/projects/jboss-cloud-support/pages/ec2-configuration-file for more info." if @ec2_data['account_number'].nil?
+      
+      # remove dashes from account number
+      @ec2_data['account_number'] = @ec2_data['account_number'].to_s.gsub(/-/, '')      
     end
     
     def define_tasks
@@ -40,8 +61,39 @@ module JBossCloud
         convert_image_to_ec2_format
       end
       
+      file @appliance_ec2_manifest_file => [ @appliance_ec2_image_file ] do
+        bundle_image
+      end
+      
+      task "testbleh" do
+        bundle_image
+      end
+      
       desc "Build #{@appliance_config.simple_name} appliance for Amazon EC2"
       task "appliance:#{@appliance_config.name}:ec2" => [ @appliance_ec2_image_file ]
+    end
+    
+    
+    def bundle_image
+      validate_config
+      
+      FileUtils.mkdir_p( @bundle_dir )
+      
+      command = "ec2-bundle-image -i #{@appliance_ec2_image_file} -c #{certs[0]} -k #{pks[0]} -u #{@nb} -r #{@config.build_arch} -d #{@bundle_dir}"
+      exit_status =  execute_command( command )
+      
+      unless exit_status
+        puts "\nBundling #{@appliance_config.simple_name} image failed! Hint: consult above messages.\n\r"
+        abort
+      end
+    end
+    
+    def upload_image
+      # ec2-upload-bundle -b [MAH_BUCKET] -m /tmp/ec2-[TIMESTAMP].img.manifest.xml -a [ACCESS_KEY] -s [SECRET_ACCESS_KEY] /tmp/ec2-[TIMESTAMP].img.manifest.xml
+    end
+    
+    def register_ami
+      # ec2-register -C [/Path/To/Cert] -K [/Path/To/Key] [MAH_BUCKET]/ec2-[TIMESTAMP].img.manifest.xml
     end
     
     def convert_image_to_ec2_format
