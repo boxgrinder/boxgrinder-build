9fe5765fcb57e2f49d7d14d1e87a27794a8fd1a8
Marek Goldmann <twoj@adres.e-mail>
2009-04-28 16:01:07 +0200

    temp release: 1.0.0.Beta5-SNAPSHOT

--- lib/jboss-cloud/defaults.rb
+++ lib/jboss-cloud/defaults.rb
@@ -66,7 +66,7 @@ module JBossCloud
   DEFAULT_PROJECT_CONFIG = {
           :name              => 'JBoss-Cloud',
           :version           => '1.0.0.Beta5',
-          :release           => '1',
+          :release           => 'SNAPSHOT',
           :dir_build         => 'build',
           #:topdir            => "#{self.} build/topdir",
   :dir_sources_cache => 'sources-cache',
