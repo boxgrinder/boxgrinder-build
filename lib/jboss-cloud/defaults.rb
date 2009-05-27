abe834393cd51dae1f22f63e97bf481423bc64ed
Marek Goldmann <marek.goldmann@gmail.com>
2009-05-27 17:33:14 +0200

    changing version

--- lib/jboss-cloud/defaults.rb
+++ lib/jboss-cloud/defaults.rb
@@ -66,7 +66,7 @@ module JBossCloud
   DEFAULT_PROJECT_CONFIG = {
           :name              => 'JBoss-Cloud',
           :version           => '1.0.0.Beta5',
-          :release           => 'SNAPSHOT',
+          :release           => '1',
           :dir_build         => 'build',
           #:topdir            => "#{self.} build/topdir",
   :dir_sources_cache => 'sources-cache',
