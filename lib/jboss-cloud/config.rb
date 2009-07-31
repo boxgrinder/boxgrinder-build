43aedf7cde339ce70e16c244acc647173938e25c
Marek Goldmann <marek.goldmann@gmail.com>
2009-07-31 15:31:57 +0200

    fix for project names with spaces

--- lib/jboss-cloud/config.rb
+++ lib/jboss-cloud/config.rb
@@ -125,7 +125,7 @@ module JBossCloud
       @dir_kickstarts   = @dir.kickstarts
 
       @aws                = OpenStruct.new
-      @aws.bucket_prefix  = "#{@name.downcase}/#{version_with_release}"
+      @aws.bucket_prefix  = "#{@name.downcase.gsub!(" ", "-")}/#{version_with_release}"
 
       @dir_base         = @dir.base
 
