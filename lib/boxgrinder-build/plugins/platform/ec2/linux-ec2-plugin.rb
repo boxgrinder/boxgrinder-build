3db887d117fdd41b910a5ad1964c341b0e528894
Marek Goldmann <marek.goldmann@gmail.com>
2010-04-23 14:00:25 +0200

    baseconverter -> baseplugin

--- lib/boxgrinder-build/plugins/platform/ec2/linux-ec2-plugin.rb
+++ lib/boxgrinder-build/plugins/platform/ec2/linux-ec2-plugin.rb
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-require 'boxgrinder-build/plugins/platform/ec2/base-converter'
+require 'boxgrinder-build/plugins/base-plugin'
 
 module BoxGrinder
   class LinuxEC2Plugin < BasePlugin
