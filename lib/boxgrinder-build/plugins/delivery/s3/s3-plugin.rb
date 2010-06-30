a2ec69ed17d643e25afd46d03688d71b0b05764e
Marek Goldmann <marek.goldmann@gmail.com>
2010-06-30 11:20:58 +0200

    added rubygems require before AWS

--- lib/boxgrinder-build/plugins/delivery/s3/s3-plugin.rb
+++ lib/boxgrinder-build/plugins/delivery/s3/s3-plugin.rb
@@ -19,6 +19,7 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
 require 'boxgrinder-build/plugins/delivery/base/base-delivery-plugin'
+require 'rubygems'
 require 'AWS'
 require 'aws/s3'
 
