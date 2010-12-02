99f1418f54ae376ce28fcb39455c1754b7c91743
Marek Goldmann <marek.goldmann@gmail.com>
2010-12-02 12:24:01 +0100

    added require 'rubygems' to some files

--- lib/boxgrinder-build/appliance.rb
+++ lib/boxgrinder-build/appliance.rb
@@ -16,6 +16,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
+require 'rubygems'
 require 'boxgrinder-core/models/appliance-config'
 require 'boxgrinder-core/models/config'
 require 'boxgrinder-core/helpers/appliance-helper'
