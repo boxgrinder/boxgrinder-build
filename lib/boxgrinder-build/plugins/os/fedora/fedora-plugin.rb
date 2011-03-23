50399c8d6a80d55e830f8a7bf860dcb555eacf3b
Tomasz Długosz <tomek3d@gmail.com>
2011-03-23 06:17:25 -0700

    use plain kernel instead of kernel-PAE on 32 bit machines with less amount of memory.

--- lib/boxgrinder-build/plugins/os/fedora/fedora-plugin.rb
+++ lib/boxgrinder-build/plugins/os/fedora/fedora-plugin.rb
@@ -50,10 +50,10 @@ module BoxGrinder
       packages << "system-config-firewall-base"
       packages << "dhclient"
       
-      # kernel_PAE for 32 bit, kernel for 64 bit
+      # kernel_PAE for 32 bit with over 4GB or RAM, kernel for 64 bit and less than 4GB or RAM
       packages.delete('kernel')
       packages.delete('kernel-PAE')
-      packages << (@appliance_config.is64bit? ? "kernel" : "kernel-PAE")
+      packages << ((@appliance_config.is64bit? or @appliance_config.hardware.memory < 4096 ) ? "kernel" : "kernel-PAE")
     end
   end
 end
