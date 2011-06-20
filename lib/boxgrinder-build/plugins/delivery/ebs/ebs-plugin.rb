94e367f5a483c039b534c07a0cd4edc9f12022ed
Marc Savy <msavy@redhat.com>
2011-06-20 21:14:40 +0100

    Show progress of copy

--- lib/boxgrinder-build/plugins/delivery/ebs/ebs-plugin.rb
+++ lib/boxgrinder-build/plugins/delivery/ebs/ebs-plugin.rb
@@ -69,109 +69,6 @@ module BoxGrinder
       register_supported_os('centos', ['5'])
     end
 
-    def get_volume_info(volume_id)
-      begin
-        @ec2.describe_volumes(:volume_id => volume_id).volumeSet.item.each do |volume|
-          return volume if volume.volumeId == volume_id
-        end
-      rescue AWS::Error #AWS::InvalidVolumeIDNotFound should be returned when no volume found, but is not at present.
-        return nil
-      end
-      nil
-    end
-
-    def snapshot_info(snapshot_id)
-     begin  
-      @ec2.describe_snapshots(:snapshot_id => snapshot_id).snapshotSet.item.each do |snapshot|
-        return snapshot if snapshot.snapshotId == snapshot_id
-      end
-     rescue AWS::InvalidSnapshotIDNotFound
-       return nil
-     end
-      nil
-    end
-
-    def block_device_from_ami(ami_info, device_name)
-      ami_info.blockDeviceMapping.item.each do |device|
-        return device if device.deviceName == device_name
-      end
-      nil
-    end
-
-    def get_instances(ami_id)
-      #EC2 Gem has yet to be updated with new filters, once the patches have been pulled then :image_id filter will be picked up
-      instances_info = @ec2.describe_instances(:image_id => ami_id).reservationSet
-      instances=[]
-      instances_info["item"].each do
-        |item| item["instancesSet"]["item"].each do |i|
-          instances.push i if i.imageId == ami_id #TODO remove check after gem update
-        end
-      end
-      return instances.uniq unless instances.empty?
-      nil
-    end
-
-    def stomp_ebs(ami_info)
-
-      device = block_device_from_ami(ami_info, ROOT_DEVICE_NAME)
-
-      if device #if there is the anticipated device on the image
-        snapshot_info = snapshot_info(device.ebs.snapshotId)
-        volume_id = snapshot_info.volumeId
-        volume_info = get_volume_info(volume_id)
-
-        @log.info "Finding any existing image with the block store attached"
-
-        if instances = get_instances(ami_info.imageId)
-          raise "There are still instances of #{ami_info.imageId} running, you must stop them: #{instances.join(",")}"
-        end
-
-        if volume_info #if the physical volume exists
-          unless volume_info.status == 'available'
-            begin
-             @log.info "Forcibly detaching block store #{volume_info.volumeId}"
-             @ec2.detach_volume(:volume_id => volume_id, :force => true)
-            rescue AWS::IncorrectState
-             @log.debug "State of the volume has changed, our data must have been stale. This should not be fatal."
-            end
-          end
-
-          @log.debug "Waiting for volume to become detached"
-          wait_for_volume_status('available', volume_info.volumeId)
-
-          begin
-            @log.info "Deleting block store"
-            @ec2.delete_volume(:volume_id => volume_info.volumeId)
-            @log.debug "Waiting for volume deletion to be confirmed"
-            wait_for_volume_delete(volume_info.volumeId)
-          rescue AWS::InvalidVolumeIDNotFound
-            @log.debug "An external entity has probably deleted the volume just before we tried to. This should not be fatal."
-          end
-        end
-
-        begin
-          @log.debug "Deregistering AMI"
-          @ec2.deregister_image(:image_id => ami_info.imageId)
-        rescue AWS::InvalidAMIIDUnavailable, AWS::InvalidAMIIDNotFound
-          @log.debug "An external entity has already deregistered the AMI just before we tried to. This should not be fatal."
-        end
-
-        if !@plugin_config['preserve_snapshots'] and snapshot_info #if the snapshot exists
-         begin
-          @log.debug "Deleting snapshot #{snapshot_info.snapshotId}"
-          @ec2.delete_snapshot(:snapshot_id => snapshot_info.snapshotId)
-         rescue AWS::InvalidSnapshotIDNotFound
-          @log.debug "An external entity has probably deleted the snapshot just before we tried to. This should not be fatal."
-         end
-        end
-
-      else
-        @log.error "Expected device #{ROOT_DEVICE_NAME} was not found, and therefore can not be unmounted"
-        return false
-      end
-      true
-    end
-
     def execute
       ebs_appliance_description = "#{@appliance_config.summary} | Appliance version #{@appliance_config.version}.#{@appliance_config.release} | #{@appliance_config.hardware.arch} architecture"
 
@@ -301,6 +198,108 @@ module BoxGrinder
       @log.info "EBS AMI '#{ebs_appliance_name}' registered: #{image_id} (region: #{@region})"
     end
 
+    def get_volume_info(volume_id)
+      begin
+        @ec2.describe_volumes(:volume_id => volume_id).volumeSet.item.each do |volume|
+          return volume if volume.volumeId == volume_id
+        end
+      rescue AWS::Error #AWS::InvalidVolumeIDNotFound should be returned when no volume found, but is not always doing so at present.
+        return nil
+      end
+      nil
+    end
+
+    def snapshot_info(snapshot_id)
+     begin
+      @ec2.describe_snapshots(:snapshot_id => snapshot_id).snapshotSet.item.each do |snapshot|
+        return snapshot if snapshot.snapshotId == snapshot_id
+      end
+     rescue AWS::InvalidSnapshotIDNotFound
+       return nil
+     end
+      nil
+    end
+
+    def block_device_from_ami(ami_info, device_name)
+      ami_info.blockDeviceMapping.item.each do |device|
+        return device if device.deviceName == device_name
+      end
+      nil
+    end
+
+    def get_instances(ami_id)
+      #EC2 Gem has yet to be updated with new filters, once the patches have been pulled then :image_id filter will be picked up
+      instances_info = @ec2.describe_instances(:image_id => ami_id).reservationSet
+      instances=[]
+      instances_info["item"].each do
+        |item| item["instancesSet"]["item"].each do |i|
+          instances.push i if i.imageId == ami_id #TODO remove check after gem update
+        end
+      end
+      return instances.uniq unless instances.empty?
+      nil
+    end
+
+    def stomp_ebs(ami_info)
+
+      device = block_device_from_ami(ami_info, ROOT_DEVICE_NAME)
+
+      if device #if there is the anticipated device on the image
+        snapshot_info = snapshot_info(device.ebs.snapshotId)
+        volume_id = snapshot_info.volumeId
+        volume_info = get_volume_info(volume_id)
+
+        @log.info "Finding any existing image with the block store attached"
+
+        if instances = get_instances(ami_info.imageId)
+          raise "There are still instances of #{ami_info.imageId} running, you must stop them: #{instances.join(",")}"
+        end
+
+        if volume_info #if the physical volume exists
+          unless volume_info.status == 'available'
+            begin
+             @log.info "Forcibly detaching block store #{volume_info.volumeId}"
+             @ec2.detach_volume(:volume_id => volume_info.volumeId, :force => true)
+            rescue AWS::IncorrectState
+             @log.debug "State of the volume has changed, our data must have been stale. This should not be fatal."
+            end
+          end
+
+          @log.debug "Waiting for volume to become detached"
+          wait_for_volume_status('available', volume_info.volumeId)
+
+          begin
+            @log.info "Deleting block store"
+            @ec2.delete_volume(:volume_id => volume_info.volumeId)
+            @log.debug "Waiting for volume deletion to be confirmed"
+            wait_for_volume_status('deleted', volume_info.volumeId)
+          rescue AWS::InvalidVolumeIDNotFound
+            @log.debug "An external entity has probably deleted the volume just before we tried to. This should not be fatal."
+          end
+        end
+
+        begin
+          @log.debug "Deregistering AMI"
+          @ec2.deregister_image(:image_id => ami_info.imageId)
+        rescue AWS::InvalidAMIIDUnavailable, AWS::InvalidAMIIDNotFound
+          @log.debug "An external entity has already deregistered the AMI just before we tried to. This should not be fatal."
+        end
+
+        if !@plugin_config['preserve_snapshots'] and snapshot_info #if the snapshot exists
+         begin
+          @log.debug "Deleting snapshot #{snapshot_info.snapshotId}"
+          @ec2.delete_snapshot(:snapshot_id => snapshot_info.snapshotId)
+         rescue AWS::InvalidSnapshotIDNotFound
+          @log.debug "An external entity has probably deleted the snapshot just before we tried to. This should not be fatal."
+         end
+        end
+      else
+        @log.error "Expected device #{ROOT_DEVICE_NAME} was not found on the image."
+        return false
+      end
+      true
+    end
+
     def ebs_appliance_name
       base_path = "#{@appliance_config.name}/#{@appliance_config.os.name}/#{@appliance_config.os.version}/#{@appliance_config.version}.#{@appliance_config.release}"
 
@@ -347,20 +346,21 @@ module BoxGrinder
       end
     end
 
-    def wait_for_volume_delete(volume_id)
-      snapshot_info = @ec2.describe_volumes(:snapshot_id => volume_id)
-      wait_with_timeout(POLL_FREQ, TIMEOUT){ snapshot_info.nil? }
-    end
-
     def wait_for_volume_attachment(suffix)
       wait_with_timeout(POLL_FREQ, TIMEOUT){ device_for_suffix(suffix) != nil }
     end
 
     def wait_for_snapshot_status(status, snapshot_id)
+      progress = -1
       wait_with_timeout(POLL_FREQ, TIMEOUT) do
-          snapshot = @ec2.describe_snapshots(:snapshot_id => snapshot_id)['snapshotSet']['item'].first
-          @log.trace "Polling @ec2.describe_snapshots for #{snapshot_id} with status #{status}: #{PP::pp(snapshot,"")}, current status is #{snapshot['status']}"
-          snapshot['status'] == status
+        snapshot = @ec2.describe_snapshots(:snapshot_id => snapshot_id)['snapshotSet']['item'].first
+        current_progress = snapshot.progress.to_i 
+        unless progress == current_progress 
+          @log.info "Progress: #{current_progress}%"
+          progress = current_progress
+        end
+        @log.trace "Polling @ec2.describe_snapshots for #{snapshot_id} with status #{status}: #{PP::pp(snapshot,"")}, current status is #{snapshot['status']}"  
+        snapshot['status'] == status
       end
     end
 
